45e93ac940cf008331a32a44f776b1e25e816439
heinminthant <heinminthant.17199@gmail.com>
2022-04-18 04:40:48 +0630

    Modified Classpack factory

--- database/factories/ClassPackFactory.php
+++ database/factories/ClassPackFactory.php
@@ -15,20 +15,20 @@ class ClassPackFactory extends Factory
     public function definition()
     {
         return [
-            'disp_order' => $this->faker->randomDigit(),
+            'disp_order' => $this->faker->unique()->numberBetween(1, 100),
             'pack_id' => Str::uuid(),
             'pack_name' => $this->faker->streetName,
             'pack_description' => $this->faker->sentence(),
             'pack_type' => $this->faker->randomElement(['unlimited', 'non_shareable']),
-            'total_credit' => $this->faker->randomDigit(),
+            'total_credit' => $this->faker->numberBetween(10, 50),
             'tag_name' => $this->faker->randomElement([null, 'New', 'Popular', 'Limited']),
-            'validity_month' => $this->faker->randomDigit(),
-            'pack_price' => $this->faker->randomDigit(),
+            'validity_month' => $this->faker->numberBetween(1, 12),
+            'pack_price' => $this->faker->numberBetween(50, 500),
             'newbie_first_attend' => $this->faker->randomElement([true, false]),
-            'newbie_addition_credit' => $this->faker->randomDigit(),
+            'newbie_addition_credit' => $this->faker->numberBetween(10, 50),
             'newbie_note' => $this->faker->sentence(),
             'pack_alias' => $this->faker->word(),
-            'estimate_price' => $this->faker->randomDigit()
+            'estimate_price' => $this->faker->numberBetween(10, 50)
         ];
     }
 }
