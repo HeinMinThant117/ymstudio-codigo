ec2bd59948a5751600ae5a5d99c99d0cb9c2359b
heinminthant <heinminthant.17199@gmail.com>
2022-04-18 03:51:23 +0630

    Added database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\ClassPack;
+use App\Models\Promocode;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(3)->create();
+        Promocode::factory(5)->create();
+        ClassPack::factory(9)->create();
     }
 }
